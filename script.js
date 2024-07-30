d64a2a8e4f1e2de9ec52aef00662959a58fed0ce
Akash <akashkant@Akashs-MacBook-Air.local>
2024-07-30 23:59:51 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let dayNight = document.querySelector(".dayNight");
+let banner = document.querySelector(".banner");
+dayNight.addEventListener("click", () =>{
+    banner.classList.toggle("night");
+})
+
+let typingEffect = new Typed("#text", {
+    strings:["Sky || Bit"],
+    loop:true,
+    typeSpeed:100,
+    backSpeed:50,
+    backDelay:1000
+});
